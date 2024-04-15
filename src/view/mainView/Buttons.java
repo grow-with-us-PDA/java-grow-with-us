2b37958a8a25063adb4b929160be2cf8f83db605
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-04-15 13:30:41 +0900

    feat: 다음 날, 상점 버튼 추가 밑 클릭 이벤트 추가

--- src/view/mainView/Buttons.java
+++ src/view/mainView/Buttons.java
@@ -2,11 +2,40 @@ package view.mainView;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Buttons extends JPanel {
+public class Buttons extends JPanel implements ActionListener {
+  String[] texts = new String[]{"다음 날", "상점"};
+  JButton[] buttons = new JButton[texts.length];
+
   Buttons() {
     setBackground(Color.YELLOW);
-    setPreferredSize(new Dimension(300, 100));
+    setPreferredSize(new Dimension(500, 110));
+    setLayout(new FlowLayout());
+
+    for (int i = 0 ; i < texts.length ; i++) {
+      buttons[i] = new JButton(texts[i]);
+
+      buttons[i].setPreferredSize(new Dimension(300, 50));
+      buttons[i].setFont(new Font("돋음", Font.PLAIN, 16));
+      buttons[i].addActionListener(this);
+
+      add(buttons[i]);
+    }
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    for (int i = 0 ; i < texts.length ; i++) {
+      if (e.getSource() == buttons[i]) {
+        // 함수 실행. 작동까지 확인함.
+      }
+    }
   }
 }
