b345671d481534adf7e46341e0c6d23c377b5785
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-04-15 10:42:07 +0900

    feat: connect View in Main

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,11 @@
 package view;
 
-public class View {
+import javax.swing.JFrame;
 
+public class View extends JFrame {
+  public View() {
+    setSize(1133, 744);
+    setLayout(null);
+    setVisible(true);
+  }
 }
