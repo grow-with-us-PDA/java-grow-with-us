1fe6fceaf1157b4e599936adcf9ab3ac6bae52f3
김영석 <kimyoungseok15@gmail.com>
2024-04-16 08:08:01 +0900

    컨트롤러 복구

--- src/view/StoreView/Seeds.java
+++ src/view/StoreView/Seeds.java
@@ -10,12 +10,14 @@ import java.awt.*;
 public class Seeds extends JPanel {
 
     Seeds(Controller controller) {
-
-        setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(1000, 600));
+        setLayout(null);
+        setPreferredSize(new Dimension(1000, 400));
         setBorder(BorderFactory.createEmptyBorder(10 , 10, 10 , 10));
-
         setBackground(Color.DARK_GRAY);
 
+        // Seeds에 추가할 내용 생성
+        JLabel seedsLabel = new JLabel("씨앗 목록");
+        seedsLabel.setForeground(Color.WHITE); // 텍스트 색상 설정
+        add(seedsLabel, BorderLayout.CENTER); // Seeds 패널에 추가
     }
 }
--- src/view/StoreView/StoreView.java
+++ src/view/StoreView/StoreView.java
@@ -1,6 +1,7 @@
 package view.StoreView;
 
 import controller.Controller;
+import model.UserModel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,23 +14,24 @@ public class StoreView extends JPanel {
     public StoreView(Controller controller) {
         this.controller = controller;
 
-        setLayout(null); // 레이아웃 매니저 설정을 제거하여 컴포넌트를 직접 배치
+        setLayout(new FlowLayout()); // 레이아웃 매니저 설정을 제거하여 컴포넌트를 직접 배치
 
         // 상점 제목 추가
         JLabel la_title = new JLabel("상점");
         la_title.setBounds(50, 100, 100, 30); // 위치와 크기 설정
         la_title.setFont(new Font("", Font.BOLD, 42));
-        add(la_title); // 패널에 추가
+        add(la_title, FlowLayout.LEFT); // 패널에 추가
 
         // 현재 보유 금액 추가
         JLabel la_money = new JLabel("현재 보유 금액: ");
         la_money.setBounds(800, 150, 150, 20); // 위치와 크기 설정
         la_title.setFont(new Font("", Font.BOLD, 30));
-        add(la_money); // 패널에 추가
+
 
         JButton goToMainButton = new JButton("메인페이지로");
         JButton goToDetailButton = new JButton("상세페이지로");
         Seeds seeds = new Seeds(controller);
+        seeds.setBounds(400, 300, 500, 200);
 
         // 각 버튼의 위치와 크기 설정
         goToMainButton.setBounds(400, 0, 100, 30);
@@ -41,11 +43,8 @@ public class StoreView extends JPanel {
 
         add(goToMainButton); // 패널에 추가
         add(goToDetailButton); // 패널에 추가
-        add(seeds);
-
-        JTextField tf_store = new JTextField("상점페이지입니다.");
-        tf_store.setBounds(600, 0, 100, 30); // 위치와 크기 설정
-        add(tf_store); // 패널에 추가
+        add(la_money, FlowLayout.RIGHT); // 패널에 추가
+        add(seeds, BorderLayout.CENTER);
 
         // 다른 UI 요소들 추가...
     }
