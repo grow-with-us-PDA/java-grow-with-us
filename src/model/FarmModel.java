202ee4ce7f738fbeaab9fee84dd7f5a7a6e91cd3
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-04-15 16:21:22 +0900

    feat : crop 사기 팔기 추가

--- src/model/FarmModel.java
+++ src/model/FarmModel.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 
 public class FarmModel {
     HashMap<Integer, CropModel> farm;
+    UserModel userModel;
 
-    public FarmModel() {
+    public FarmModel(UserModel userModel) {
         farm = new HashMap<>();
+        this.userModel = userModel;
         System.out.println("farm 초기화");
     }
 
@@ -15,7 +17,7 @@ public class FarmModel {
         if (farm.containsKey(location)) {
             System.out.println("이미 작물이 있습니다.");
         } else {
-            System.out.println("작물 심기 완료");
+            System.out.println("위치 " + location + "에 작물 심기 완료");
             farm.put(location, crop);
         }
     }
@@ -34,12 +36,25 @@ public class FarmModel {
 
     public void removeCropAtFarm(int location) { // 해당 location에 crop 삭제
         if (farm.containsKey(location)) {
-            System.out.println("작물 삭제 완료");
+            System.out.println("위치 " + location + "에 작물 삭제 완료");
             farm.remove(location);
         } else {
             System.out.println("해당 작물이 없습니다.");
         }
     }
+
+    public void buyCorn() { // corn 사기
+        Corn corn = new Corn();
+        this.userModel.setMoney(-corn.seedPrice);
+        setCropAutoAtFarm(corn);
+    }
+
+    public void sellCrop(int location) { // location에 있는 작물 팔기
+        CropModel crop = farm.get(location);
+        removeCropAtFarm(location);
+        this.userModel.setMoney(crop.sellPrice);
+    }
+
 
     public HashMap<Integer, CropModel> getFarm() { // 현재 farm 상태 반환
         return this.farm;
