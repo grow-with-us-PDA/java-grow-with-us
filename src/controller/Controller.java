20cc37ddedc2673fe9babe97af03ba7fcb28a342
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-04-16 11:11:20 +0900

    chore: 로직 주석 추가

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,15 @@
 package controller;
 
+import model.Corn;
+import model.CropModel;
 import model.DateModel;
+import model.FarmModel;
 import model.StoreModel;
 import model.WeatherModel;
 import view.View;
 
 public class Controller {
-//  public DetailController detailController;
-
+    public DetailController detailController;
     public StoreController storeController = new StoreController(new StoreModel());
     public MainPageController mainPageController;
     View view;
@@ -17,7 +19,11 @@ public class Controller {
 
       WeatherModel weatherModel = new WeatherModel();
       DateModel dateModel =  new DateModel();
+
+//      Corn corn = new Corn(dateModel);
       this.mainPageController = new MainPageController(weatherModel, dateModel);
+
+
     }
 
     public void goToStorePage() {
