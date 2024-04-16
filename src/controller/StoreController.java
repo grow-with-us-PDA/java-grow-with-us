4fd69793b47ee4c62c1a4e30d398ed52890840e2
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-04-16 17:27:54 +0900

    feat : 팔기

--- src/controller/StoreController.java
+++ src/controller/StoreController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.CropModel;
 import model.StoreModel;
 
 import java.util.Arrays;
@@ -41,5 +42,9 @@ public class StoreController {
 
     public boolean buyCropBySeedIndex(int seedIndex){
         return this.storeModel.buyCropBySeedIndex(seedIndex);
+    }
+
+    public void sellCropByCrop(CropModel cropModel){
+        this.storeModel.getFarmModelByStoreModel().sellCropByCrop(cropModel);
     }
 }
--- src/model/FarmModel.java
+++ src/model/FarmModel.java
@@ -48,7 +48,7 @@ public class FarmModel {
     }
 
     public boolean getCanSeed() {
-        if(this.farm.size() == 9){
+        if (this.farm.size() == 9) {
             return false;
         }
         return true;
@@ -66,7 +66,7 @@ public class FarmModel {
 
     public boolean buyCropByStore(CropModel cropModel) {
         boolean canSeed = getCanSeed();
-        if(!canSeed){
+        if (!canSeed) {
             return false;
         }
         boolean isSetMoney = this.userModel.setMoney(-cropModel.seedPrice);
@@ -86,6 +86,13 @@ public class FarmModel {
         CropModel crop = farm.get(location);
         removeCropAtFarm(location);
         this.userModel.setMoney(crop.sellPrice);
+    }
+
+    public void sellCropByCrop(CropModel cropModel) {
+        int locationByCrop = getKeyFromValue(cropModel);
+        System.out.println("L :"+locationByCrop);
+        removeCropAtFarm(locationByCrop);
+        this.userModel.setMoney(cropModel.sellPrice);
     }
 
     public CropModel getCropByLocation(int location) { // 해당 location의 crop 가져오기
@@ -122,12 +129,21 @@ public class FarmModel {
         return this.farm;
     }
 
-    public static <K, V> K getKeyFromValue(HashMap<K, V> map, V value) {
-        for (HashMap.Entry<K, V> entry : map.entrySet()) {
-            if (value.equals(entry.getValue())) {
+//    public static <K, V> K getKeyFromValue(HashMap<K, V> map, V value) {
+//        for (HashMap.Entry<K, V> entry : map.entrySet()) {
+//            if (value.equals(entry.getValue())) {
+//                return entry.getKey();
+//            }
+//        }
+//        return null;
+//    }
+
+    public Integer getKeyFromValue(CropModel cropModel) {
+        for (HashMap.Entry<Integer, CropModel> entry : farm.entrySet()) {
+            if (cropModel.equals(entry.getValue())) {
                 return entry.getKey();
             }
         }
-        return null;
+        return null; // Return null if the cropModel is not found in the map
     }
 }
